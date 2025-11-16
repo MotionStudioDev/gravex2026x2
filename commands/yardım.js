d75cab29c522ab72bbcf4a0fece6cee7b405f7b4
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-11-16 19:59:20 +0300

    Update yardım.js

--- commands/yardım.js
+++ commands/yardım.js
@@ -1,1 +1,13 @@
-ssad
+const { ButtonStyle } = require('discord.js')
+const db = require("orio.db");
+const { EmbedBuilder, ButtonBuilder, ActionRowBuilder } = require("discord.js");
+let config = require(`../config.js`)
+module.exports.run = async (client, message, args) => {
+  message.channel.send('sa')
+};
+module.exports.conf = {
+  aliases: []
+};
+module.exports.help = {
+  name: "yardım"
+};
