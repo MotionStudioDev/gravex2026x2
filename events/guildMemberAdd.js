89d5c92504acd886e065bee51045eb3e1a87677f
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-11-16 22:40:17 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -10,21 +10,29 @@ module.exports = async (member) => {
   const mevcut = member.guild.memberCount;
   const kalan = hedef - mevcut;
 
+  const embed = new EmbedBuilder()
+    .setColor('Green')
+    .setTitle('👤 Yeni Üye Katıldı')
+    .setDescription(`**${member.user.tag}** aramıza katıldı!\nHedefe ulaşmak için **${kalan}** kişi kaldı.`)
+    .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+    .setFooter({ text: 'Sayaç sistemi' });
+
+  const kanalId = client.sayaçKanalları?.get(guildId);
+  const kanal = kanalId
+    ? member.guild.channels.cache.get(kanalId)
+    : member.guild.systemChannel;
+
+  if (kanal && kanal.permissionsFor(client.user).has('SendMessages')) {
+    kanal.send({ embeds: [embed] });
+  }
+
   if (kalan <= 0) {
-    const embed = new EmbedBuilder()
-      .setColor('Green')
+    const kutlama = new EmbedBuilder()
+      .setColor('Gold')
       .setTitle('🎉 Sayaç Tamamlandı!')
-      .setDescription(`Sunucumuz **${hedef}** üyeye ulaştı!\nHoş geldin ${member}, seni aramızda görmek harika!`)
-      .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-      .setFooter({ text: 'Sayaç sistemi' });
+      .setDescription(`Sunucumuz **${hedef}** üyeye ulaştı!\nHoş geldin ${member}, seni aramızda görmek harika!`);
 
-    const kanalId = client.sayaçKanalları?.get(guildId);
-    const kanal = kanalId
-      ? member.guild.channels.cache.get(kanalId)
-      : member.guild.systemChannel || member.guild.channels.cache.find(c => c.type === 0 && c.permissionsFor(client.user).has('SendMessages'));
-
-    if (kanal) kanal.send({ embeds: [embed] });
-
+    kanal?.send({ embeds: [kutlama] });
     client.sayaçlar.delete(guildId);
     client.sayaçKanalları.delete(guildId);
   }
