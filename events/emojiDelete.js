8d7879ed8cfb3ddd4a8e8e2ed4cf10badb177e87
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-11-17 16:16:35 +0300

    Create emojiDelete.js

---
+++ events/emojiDelete.js
@@ -0,0 +1,21 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = async (emoji) => {
+  const client = emoji.client;
+  const kanalId = client.emojiLogKanalları?.get(emoji.guild.id);
+  const kanal = kanalId ? emoji.guild.channels.cache.get(kanalId) : null;
+  if (!kanal || !kanal.permissionsFor(client.user).has('SendMessages')) return;
+
+  const embed = new EmbedBuilder()
+    .setColor('Red')
+    .setTitle('➖ Emoji Silindi')
+    .addFields(
+      { name: 'İsim', value: emoji.name, inline: true },
+      { name: 'Animasyonlu muydu?', value: emoji.animated ? 'Evet' : 'Hayır', inline: true },
+      { name: 'ID', value: emoji.id, inline: false }
+    )
+    .setTimestamp()
+    .setFooter({ text: 'Emoji Log' });
+
+  kanal.send({ embeds: [embed] });
+};
