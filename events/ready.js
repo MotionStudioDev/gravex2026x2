2d720c295f8ef0bdf56870a137cd85bfe2d78ea6
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-11-17 18:32:06 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,27 +1,30 @@
-const { ActivityType, EmbedBuilder } = require('discord.js');
-const chalk = require('chalk');
+const client = require("../main");
+const { Collection } = require("discord.js")
+const fs = require("fs")
+const db = require('orio.db')
+client.on("ready", () => {
+console.log(`${client.user.tag} Aktif!`)
+  let x = [
+    `g!yardım - g!davet - Grave v1.0.0`
+  ]
+  let q = x[Math.floor(Math.random() * x.length)]
+client.user.setActivity(q)
 
-module.exports = async (client) => {
-  console.log(chalk.green(`[READY] ${client.user.tag} başarıyla giriş yaptı.`));
+client.commands = new Collection();
+client.aliases = new Collection();
+fs.readdir("./commands/", (err, files) => {
+if (err) console.error(err);
+console.log(`Toplam ${files.length} komut var!`);
+files.forEach(f => {
+let props = require(`../commands/${f}`);
+    
+console.log(`${props.help.name}.js Komutu aktif!`);
+    
+client.commands.set(props.help.name, props);
+props.conf.aliases.forEach(alias => {
+client.aliases.set(alias, props.help.name);
+});
+});
+});
 
-  client.user.setStatus('online');
-
-  const durumlar = [
-    `g!yardım | Grave v1.0.0`,
-    `${client.guilds.cache.size} sunucu | ${client.users.cache.size} kullanıcı`,
-    `g!premium | g!webpanel | g!gold`,
-    `BETA 0.8.4 | Grave`
-  ];
-
-  let index = 0;
-  setInterval(() => {
-    const durum = durumlar[index++ % durumlar.length];
-    client.user.setActivity(durum, { type: ActivityType.Watching });
-  }, 10000);
-
-  const toplamKanal = client.channels.cache.size;
-  const toplamSunucu = client.guilds.cache.size;
-  const toplamKullanıcı = client.guilds.cache.reduce((a, g) => a + g.memberCount, 0);
-
-  console.log(chalk.blue(`[INFO] ${toplamSunucu} sunucu, ${toplamKanal} kanal, ${toplamKullanıcı.toLocaleString()} kullanıcıya hizmet veriliyor.`));
-};
+});
