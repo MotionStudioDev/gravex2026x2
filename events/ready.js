b18cb8ef835cc1d160872facf0fb7f581a232558
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-11-17 18:18:30 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,30 +1,23 @@
-const client = require("../main");
-const { Collection } = require("discord.js")
-const fs = require("fs")
-const db = require('orio.db')
-client.on("ready", () => {
-console.log(`${client.user.tag} Aktif!`)
-  let x = [
-    `g!yardım - g!davet - Grave v1.0.0`
-  ]
-  let q = x[Math.floor(Math.random() * x.length)]
-client.user.setActivity(q)
+const chalk = require('chalk');
+const moment = require('moment');
+const { ActivityType } = require('discord.js');
+const ayarlar = require('../ayarlar.json');
 
-client.commands = new Collection();
-client.aliases = new Collection();
-fs.readdir("./commands/", (err, files) => {
-if (err) console.error(err);
-console.log(`Toplam ${files.length} komut var!`);
-files.forEach(f => {
-let props = require(`../commands/${f}`);
-    
-console.log(`${props.help.name}.js Komutu aktif!`);
-    
-client.commands.set(props.help.name, props);
-props.conf.aliases.forEach(alias => {
-client.aliases.set(alias, props.help.name);
-});
-});
-});
+const prefix = ayarlar.prefix;
 
-});
+module.exports = (client) => {
+  console.log(`${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus('online'); // dnd, idle, invisible, online
+
+  client.user.setActivity(
+    `!YENİYIL🔥 !premium🔥 !steam🔥 !webpanel🔥 !yardım🔥 + !gold🔥 + BETA 0.8.4`,
+    { type: ActivityType.Watching }
+  );
+
+  const toplamKanal = client.channels.cache.size;
+  const toplamSunucu = client.guilds.cache.size;
+  const toplamKullanıcı = client.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0);
+
+  console.log(`${client.user.id}`);
+  console.log(`${client.user.username}: Şu an ${toplamKanal} adet kanala, ${toplamSunucu} adet sunucuya ve ${toplamKullanıcı.toLocaleString()} kullanıcıya hizmet veriliyor!`);
+};
