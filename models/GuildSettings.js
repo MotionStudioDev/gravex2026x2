4d9c6b25d61a3fa168b07efe97744835f428b37b
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-11-21 16:41:19 +0300

    Update GuildSettings.js

--- models/GuildSettings.js
+++ models/GuildSettings.js
@@ -5,7 +5,8 @@ const guildSettingsSchema = new mongoose.Schema({
   kufurEngel: { type: Boolean, default: false },
   kufurLog: { type: String, default: null },
   reklamEngel: { type: Boolean, default: false },
-  reklamLog: { type: String, default: null }
+  reklamLog: { type: String, default: null },
+  sesLog: { type: String, default: null } // ✅ Ses sistemi log kanalı
 });
 
 module.exports = mongoose.model('GuildSettings', guildSettingsSchema);
