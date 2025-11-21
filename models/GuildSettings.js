2240d1cef92c9d2518e6b05568ed526480814e71
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-11-21 16:26:29 +0300

    Update GuildSettings.js

--- models/GuildSettings.js
+++ models/GuildSettings.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const guildSettingsSchema = new mongoose.Schema({
   guildId: { type: String, required: true, unique: true },
   kufurEngel: { type: Boolean, default: false },
-  kufurLog: { type: String, default: null }
+  kufurLog: { type: String, default: null },
+  reklamEngel: { type: Boolean, default: false },
+  reklamLog: { type: String, default: null }
 });
 
 module.exports = mongoose.model('GuildSettings', guildSettingsSchema);
