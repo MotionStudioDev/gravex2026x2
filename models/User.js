aa613e1b4a9ce29e9846a93bf27f00ceba76d913
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-11-29 15:10:27 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   id: { type: String, required: true, unique: true },
   wallet: { type: Number, default: 0 },
-  bank: { type: Number, default: 0 }
+  bank: { type: Number, default: 0 },
+  job: { type: String, default: null },
+  lastWork: { type: Date, default: null }
 });
 
 module.exports = mongoose.model("User", userSchema);
