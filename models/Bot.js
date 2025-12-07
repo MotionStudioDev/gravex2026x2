cc2e8597fdd1901bb0f6de0d781816f241784375
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-12-07 12:11:09 +0300

    Create Bot.js

---
+++ models/Bot.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const botSchema = new mongoose.Schema({
+    botId: { type: String, required: true, unique: true }, // Botun ID'si
+    ownerId: { type: String, required: true }, // Bot sahibinin ID'si
+    prefix: { type: String, required: true }, // Botun prefix'i
+    shortDescription: { type: String, required: true, maxlength: 100 }, // Kısa açıklama
+    longDescription: { type: String, required: true, maxlength: 1000 }, // Uzun açıklama
+    inviteUrl: { type: String, required: true }, // Davet linki
+    status: { type: String, default: 'Pending' }, // Pending, Approved, Denied
+    addedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Bot', botSchema);
