3666a25187baafbb3bb15a8f382b1f77e8aa5369
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-12-12 21:11:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -9,10 +9,6 @@ const { Collection } = require("discord.js");
 const config = require("./config.js");
 const fs = require("fs");
 const db = require("orio.db");
-
-const ffmpegPath = require('ffmpeg-static');
-process.env.FFMPEG_PATH = ffmpegPath;
-
 const Discord = require("discord.js")
 const moment = require('moment')
 require('moment-duration-format')
