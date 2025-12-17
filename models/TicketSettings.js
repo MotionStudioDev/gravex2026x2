545b4641ac3b8a830ab2dd47b8376982ef636b86
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-12-17 20:03:20 +0300

    Update TicketSettings.js

--- models/TicketSettings.js
+++ models/TicketSettings.js
@@ -4,7 +4,6 @@ const TicketSettingsSchema = new mongoose.Schema({
     guildId: { type: String, required: true, unique: true },
     staffRoleId: { type: String },
     categoryId: { type: String },
-    logChannelId: { type: String }
-});
+    logChannelId: { type: String, default: null }});
 
 module.exports = mongoose.model('TicketSettings', TicketSettingsSchema);
