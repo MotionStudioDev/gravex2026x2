e1de605fa6c57e0816b7f551af4196ff54f0a701
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2025-12-21 21:20:41 +0300

    Create ses2.js

---
+++ commands/ses2.js
@@ -0,0 +1,37 @@
+const { createAudioPlayer, createAudioResource, joinVoiceChannel, AudioPlayerStatus, StreamType } = require('@discordjs/voice');
+const axios = require('axios');
+
+module.exports.run = async (client, message, args) => {
+  const voiceChannel = message.member.voice.channel;
+  if (!voiceChannel) return message.reply('❌ Sesi dinletmem için kanalda olmalısın!');
+
+  // Kanalda atılan son pcm dosyasını bul
+  const messages = await message.channel.messages.fetch({ limit: 20 });
+  const audioMsg = messages.find(m => m.attachments.first() && m.attachments.first().name.endsWith('.pcm'));
+
+  if (!audioMsg) return message.reply('❌ Yakın zamanda kaydedilmiş bir ses dosyası bulamadım!');
+
+  const connection = joinVoiceChannel({
+    channelId: voiceChannel.id,
+    guildId: message.guild.id,
+    adapterCreator: message.guild.voiceAdapterCreator,
+  });
+
+  const url = audioMsg.attachments.first().url;
+  const response = await axios.get(url, { responseType: 'stream' });
+  
+  const player = createAudioPlayer();
+  const resource = createAudioResource(response.data, { inputType: StreamType.Raw });
+
+  player.play(resource);
+  connection.subscribe(player);
+
+  message.reply('🔊 Ses kaydı oynatılıyor...');
+
+  player.on(AudioPlayerStatus.Idle, () => {
+    connection.destroy();
+  });
+};
+
+module.exports.conf = { aliases: ['dinle'] };
+module.exports.help = { name: 'ses-dinle' };
