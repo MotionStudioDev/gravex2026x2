9424fbfde34fcaf90a20d52edbf55c92d8c673fa
MotionStudio <62178936+MotionStudioDev@users.noreply.github.com>
2026-01-20 15:54:02 +0300

    Update SpamLog.js

--- models/SpamLog.js
+++ models/SpamLog.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+
 const spamLogSchema = new mongoose.Schema({
-    guildId: String,
-    userId: String,
-    ihlalSayisi: { type: Number, default: 0 }
+    guildId: { type: String, required: true },
+    userId: { type: String, required: true },
+    ihlalSayisi: { type: Number, default: 0 },
+    sonIhlal: { type: Date, default: Date.now }
 });
+
 module.exports = mongoose.model('SpamLog', spamLogSchema);
